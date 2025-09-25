cc55e75976443a37b348ffe301a14b53afc3bbc3
rylander925 <rylander925@gmail.com>
2025-09-24 21:07:02 -0700

    created colors in main

--- main.cpp
+++ main.cpp
@@ -29,11 +29,39 @@ class Color {
 };
 
 int main() {
+    Color red, orange, yellow, green, blue, purple, white, black;
+    red.setR(255);
 
+    orange.setR(255);
+    orange.setG(150);
+
+    yellow.setR(200);
+    yellow.setG(255);
+
+    green.setG(255);
+
+    blue.setB(255);
+
+    purple.setR(175);
+    purple.setB(175);
+
+    white.setR(255);
+    white.setG(255);
+    white.setB(255);
+
+    red.print();
+    orange.print();
+    yellow.print();
+    green.print();
+    blue.print();
+    purple.print();
+    white.print();
+    black.print();
 }
 
 void Color::print() {
     cout << "R: " << r << endl;
     cout << "G: " << g << endl;
     cout << "B: " << b << endl;
+    cout << endl;
 }
